3729c8b41309455e9104324b54c702e5316ac324
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2021-09-14 18:24:07 +0300

    fix bugs with input

--- src/js/components/fetchMovieByInput.js
+++ src/js/components/fetchMovieByInput.js
@@ -1,30 +1,32 @@
-
 import api from '../services/api';
-import renderMovis from '../../templates/renderMovis.hbs'
-const inputRef = document.querySelector('.input');
-const ulRef = document.querySelector('#gallary-list')
-async function getMovieData() {
+import renderMovis from '../../templates/renderMovis.hbs';
+const inputRef = document.querySelector('.input__form');
+
+const ulRef = document.querySelector('#gallary-list');
   try {
-    inputRef.addEventListener('input', onInput)
+    inputRef.addEventListener('input', onInput);
     async function onInput(e) {
-        e.preventDefault()
+      e.preventDefault();
       const inputText = e.target.value;
-      if (inputText !== "") {
-        const { results: data } = await api.fetchMovieByInput(inputText)
-          console.log(data);
-        cardMarkup(data)
-          console.log(data);
+      console.log(inputText);
+      if (inputText !== '') {
+        const { results: data } = await api.fetchMovieByInput(inputText);
+        cardMarkup(data);
       }
-};
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
 
 function cardMarkup(data) {
-  const makeMarkup = dataPrepareToRender(data)
-    ulRef.innerHTML = renderMovis(makeMarkup)
+  const makeMarkup = dataPrepareToRender(data);
+  ulRef.innerHTML = renderMovis(makeMarkup);
 }
 
 function dataPrepareToRender(data) {
   const dataToRender = data.map(movie => {
-    const { genre_ids, title, vote_average, release_date, poster_path } = movie;
+    const { genre_ids, title, vote_average, release_date, poster_path, id } = movie;
 
     const date = new Date(release_date);
 
@@ -34,6 +36,7 @@ function dataPrepareToRender(data) {
       title,
       vote_average,
       release_date: date.getFullYear(),
+      id,
     };
   });
   return dataToRender;
