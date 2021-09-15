cc635674adddfb4a797493242c6c41f679db68a3
Ivan Markushyn <vanuhamark@gmail.com>
2021-09-15 13:52:24 +0300

    add notiflix

--- src/js/components/fetchMovieByInput.js
+++ src/js/components/fetchMovieByInput.js
@@ -2,6 +2,7 @@ import api from '../services/api';
 import renderMovis from '../../templates/renderMovis.hbs';
 import dataPrepareToRender from '../services/renderCard';
 import fetchMovieByTrending from './fetchMovieByTrending';
+import Notiflix from "notiflix";
 var debounce = require('lodash.debounce');
 
 const inputRef = document.querySelector('.input__form');
@@ -16,6 +17,9 @@ try {
       const { genres } = await api.fetchGenres();
       const { results: data } = await api.fetchMovieByInput(inputText);
       cardMarkup(data, genres);
+      if (data.length === 0) {
+        Notiflix.Notify.failure('Search result not successful. Enter the correct movie name and ');
+      }
     }
     if (inputText === '') {
       fetchMovieByTrending();
@@ -26,7 +30,15 @@ try {
 }
 
 async function cardMarkup(data, genres) {
-  const makeMarkup =await dataPrepareToRender(data, genres);
-  console.log(makeMarkup);
+  const makeMarkup = await dataPrepareToRender(data, genres);
   ulRef.innerHTML = renderMovis(makeMarkup);
 }
+Notiflix.Notify.init({
+  width: "400px",
+  distance: "140px",
+  fontSize: "12px",
+  useIcon: false,
+  // closeButton: true,
+  timeout: '4000',
+  position: "center-top",
+});
