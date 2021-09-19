369bc6b38e45c037692ee4dd5bdc5df28178908a
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2021-09-19 15:20:30 +0300

    add basicLightBox

--- src/js/components/trailer.js
+++ src/js/components/trailer.js
@@ -7,13 +7,17 @@ trailerPlay.addEventListener('click', () => onShowTrailer(550));
 
 async function onShowTrailer(id) {
   const result = await api.fetchTrailer(id);
-  console.log(result.results[0].key);
+  console.log(result.results);
   if (!result.results) {
     return;
   }
 
   const instance = basicLightbox.create(`
-    <iframe src="https://www.youtube.com/embed/${result.results[0].key}?autoplay=1&origin" width="560" height="315" frameborder="0"></iframe>
-`);
+      <iframe src="https://www.youtube.com/embed/${result.results[0].key}?autoplay=1&origin" width="560" height="315" frameborder="0"></iframe>
+  `);
+
+  document.body.classList.remove('body-modal-open');
+  api.refs.galleryList.classList.add('visually-hidden');
+  api.refs.modal.classList.add('visually-hidden');
   instance.show();
 }
